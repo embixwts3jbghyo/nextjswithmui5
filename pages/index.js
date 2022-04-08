60152b3ae2945604430368bd44a6846399c20345
embixwts3jbghyo <embixwts3jbghyo@upload.pipedream.net>
2022-04-08 15:10:44 +0000

    Add new serverless func

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 
 
-import { Container} from "@mui/material";
+import { Container, Autocomplete,TextField } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-
+  const [top100Films, setTop100Films] = useState(null)
+  useEffect(() => {
+    fetch('/api/books')
+      .then(res => res.json())
+      .then(data => {
+        setTop100Films(data)
+      })
+  }, [])
   return (
     <Container maxWidth="sm" sx={{ marginTop: '10px' }}>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={top100Films}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Movie" />}
+      />
+      
     </Container>
   );
 }
